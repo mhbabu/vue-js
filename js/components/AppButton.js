71d6907ea0467780324a25d986ca81beb40c5855
mhbabu <write2babu.cse@gmail.com>
2022-10-25 18:38:07 +0600

    first commit

---
+++ js/components/AppButton.js
@@ -0,0 +1,21 @@
+export default {
+    template: `<button :class="{
+        'border rounded px-5 py-2 disabled:cursor-not-allowed': true,
+        'bg-blue-200 hover:bg-blue-400': type === 'primary',
+        'bg-purple-200 hover:bg-purple-400': type === 'secondary',
+        'bg-green-200 hover:bg-green-400': type === 'info',
+    }" 
+    :disabled="processing"><slot/></button>`,
+
+    props: {
+
+        type: {
+            type: String,
+            default: 'primary'
+        }
+    },
+
+    data() {
+        return { processing: true }
+    }
+}
